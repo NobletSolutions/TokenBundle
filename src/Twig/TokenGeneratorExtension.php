998547121a2e4a7ac3923a3836a718edcc26ae5c
Nathanael d. Noblet <nathanael@noblet.ca>
2020-06-25 12:22:12 -0600

    Removed updated twig functions/classes

--- src/Twig/TokenGeneratorExtension.php
+++ src/Twig/TokenGeneratorExtension.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace NS\TokenBundle\Twig;
 
+use Lcobucci\JWT\Token;
 use NS\TokenBundle\Generator\LongTokenException;
 use NS\TokenBundle\Generator\TokenGenerator;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
-class TokenGeneratorExtension extends \Twig_Extension
+class TokenGeneratorExtension extends AbstractExtension
 {
     /** @var TokenGenerator */
     private $generator;
@@ -16,8 +20,6 @@ class TokenGeneratorExtension extends \Twig_Extension
     private $long;
 
     /**
-     * TwigTokenGeneratorExtension constructor.
-     *
      * @param TokenGenerator $generator
      * @param int $short
      * @param int $long
@@ -35,9 +37,9 @@ class TokenGeneratorExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('generate_token', [$this, 'generateToken'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction('generate_short_token', [$this, 'generateShortToken'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction('generate_long_token', [$this, 'generateLongToken'], ['is_safe' => ['html']]),
+            new TwigFunction('generate_token', [$this, 'generateToken'], ['is_safe' => ['html']]),
+            new TwigFunction('generate_short_token', [$this, 'generateShortToken'], ['is_safe' => ['html']]),
+            new TwigFunction('generate_long_token', [$this, 'generateLongToken'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -46,7 +48,7 @@ class TokenGeneratorExtension extends \Twig_Extension
      * @param $email
      * @param array|null $extraData
      *
-     * @return \Lcobucci\JWT\Token
+     * @return Token
      */
     public function generateShortToken($id, $email, array $extraData = null)
     {
@@ -59,7 +61,7 @@ class TokenGeneratorExtension extends \Twig_Extension
      * @param $email
      * @param array|null $extraData
      *
-     * @return \Lcobucci\JWT\Token
+     * @return Token
      */
     public function generateLongToken($id, $email, array $extraData = null)
     {
@@ -72,7 +74,7 @@ class TokenGeneratorExtension extends \Twig_Extension
      * @param $email
      * @param array|null $extraData
      *
-     * @return \Lcobucci\JWT\Token
+     * @return Token
      */
     public function generateToken($id, $email, array $extraData = null)
     {
