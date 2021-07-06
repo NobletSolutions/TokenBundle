ee20311dd8ab6642d60bed21b6fdfd21309a5730
Nathanael d. Noblet <nathanael@noblet.ca>
2021-07-06 12:39:58 -0600

    Removed SF3 config BC compat

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -8,15 +8,9 @@ use Lcobucci\JWT\Signer\Hmac\Sha256;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
-        // Instantiating a new TreeBuilder without a constructor arg is deprecated in SF4 and removed in SF5
-        if (method_exists(TreeBuilder::class, '__construct')) {
-            $treeBuilder = new TreeBuilder('ns_token');
-        } else { // Included for backward-compatibility with SF3
-            $treeBuilder = new TreeBuilder();
-            $treeBuilder->root('ns_token');
-        }
+        $treeBuilder = new TreeBuilder('ns_token');
 
         $treeBuilder
             ->getRootNode()
